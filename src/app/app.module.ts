74509255f0ddaaf51d7b0d8000bc8d98b304e4ed
HamiltonGravito <hamiltongravito@gmail.com>
2017-12-07 16:39:45 -0200

    Criação dos componentes painel, progresso, tentativas e topo.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component';
+import { PainelComponent } from './painel/painel.component';
+import { ProgressoComponent } from './progresso/progresso.component';
+import { TentativasComponent } from './tentativas/tentativas.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent,
+    PainelComponent,
+    ProgressoComponent,
+    TentativasComponent
   ],
   imports: [
     BrowserModule
